d2348edbd2b26df397b533afb35decf4c567c9eb
zhishi <1951671751@qq.com>
2026-04-16 10:31:29 +0800

    去除log

--- src/stores/productionAgent.ts
+++ src/stores/productionAgent.ts
@@ -66,7 +66,6 @@ function makeProductionAgentStore(projectId: string) {
           flowData.value.storyboardTable = value ?? "";
         } else if (tag === "storyboardItem") {
           if (status === "complete") {
-            console.log("%c Line:70 🍒 attrs", "background:#93c0a4", attrs);
 
             const prompt = attrs.prompt ?? "";
             const duration = Number(attrs.duration) || 0;
@@ -76,7 +75,6 @@ function makeProductionAgentStore(projectId: string) {
                 String(attrs.shouldGenerateImage).toLowerCase() == "true"
                 ? 1
                 : 0;
-            console.log("%c Line:75 🥤 shouldGenerateImage", "background:#33a5ff", shouldGenerateImage);
 
             const videoDesc = attrs?.videoDesc ?? "";
             const existingIndex = flowData.value.storyboard.findIndex(
