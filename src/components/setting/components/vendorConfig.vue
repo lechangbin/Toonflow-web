c34756c09b80d14aa863b4f24b1f05a3699983bb
ACT丶流星雨 <1340145680@qq.com>
2026-04-19 11:37:44 +0800

    修正新增供应商的排序问题

--- src/components/setting/components/vendorConfig.vue
+++ src/components/setting/components/vendorConfig.vue
@@ -267,14 +267,13 @@
       :maskClosable="false">
       <div class="data">
         <t-radio-group variant="default-filled" v-model="addMode">
-          <t-radio-button value="codeAdd">通过代码添加</t-radio-button>
           <t-radio-button value="importAdd">通过文件导入</t-radio-button>
           <t-radio-button value="linkAdd">通过链接添加</t-radio-button>
+          <t-radio-button value="codeAdd">通过代码添加</t-radio-button>
         </t-radio-group>
         <div class="linkAdd" v-if="addMode == 'linkAdd'">
           <t-alert theme="warning" style="margin-bottom: 20px">
-            请填写 TypeScript 代码文件的链接（.ts 文件），不要填 API 地址或其他无关链接。
-            确认后 Toonflow 会自动加载该代码，请确保链接来源可信。
+            请填写 TypeScript 代码文件的链接（.ts 文件），不要填 API 地址或其他无关链接。 确认后 Toonflow 会自动加载该代码，请确保链接来源可信。
           </t-alert>
           <t-input v-model="link" :placeholder="$t('settings.vendor.linkAddPlaceholder')"></t-input>
           <div style="margin-top: 10px; text-align: right; width: 100%">
@@ -645,11 +644,11 @@ watch(
 );
 const id = ref<string>();
 function handleAddVendor() {
-  addMode.value = "codeAdd";
+  addMode.value = "importAdd";
   id.value = undefined;
   vendorCode.value = VENDOR_CODE_TEMPLATE;
   vendorDialogVisible.value = true;
-  codeDialogVisible.value = true
+  codeDialogVisible.value = false;
 }
 function handleConfirmVendor() {
   if (!id.value) {
@@ -1087,7 +1086,7 @@ function onChange(item: any, val: number) {
       item.enable = prevEnable;
     });
 }
-const addMode = ref("codeAdd");
+const addMode = ref("importAdd");
 const link = ref("");
 const linkReading = ref(false);
 
